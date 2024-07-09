25f7f5d591c6b17fc5e208f7d29b1debcab691f2
BoyPhanna <kimphanna56@gmail.com>
2024-07-09 23:07:36 +0700

    version3

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	jwtware "github.com/gofiber/jwt/v2"
 	"github.com/golang-jwt/jwt/v4"
 	"gorm.io/driver/postgres"
@@ -53,7 +54,12 @@ func main() {
 
 	// Setup Fiber
 	app := fiber.New()
-
+	// Apply CORS middleware
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*", // Adjust this to be more restrictive if needed
+		AllowMethods: "GET,POST,HEAD,PUT,DELETE,PATCH",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 	// JWT Secret Key
 	secretKey := "secret"
 
